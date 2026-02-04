52e99b192dc92c94615d7e9fefe595d7ef09a3c8
santinoarevalo66-hash <santinoarevalo66@gmail.com>
2026-02-04 14:20:24 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,4 +3,8 @@ public class Main {
 
          Viewer viewer = new Viewer(1, "Aldrian");
         Movie movie = new Movie(123, "Hello Love Goodbye", 400.00);
+        Ticket ticket = new Ticket(1001, viewer, movie);
+
+        System.out.println(ticket.fullDetails());
+    }
 }

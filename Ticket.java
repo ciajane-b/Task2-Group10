3728d47c52d6ddfa2ee3a884cdcf513d520c0008
ciajane-b <ciarajanebarro22@gmail.com>
2026-02-04 14:12:56 +0800

    Enhance Ticket class with identification and total calculation
    Add methods to identify the Ticket class and calculate total price.

--- Ticket.java
+++ Ticket.java
@@ -8,4 +8,22 @@ public class Ticket {
         this.viewer = viewer;
         this.movie = movie;
     }
+    
+    public String identify() {
+        return "I am the Ticket Class. I coordinate the viewer and movie data to process the sale.";
+    }
+    
+    public double calculateTotal() {
+        return movie.getPrice();
+    }
+    
+    public String fullDetails() {
+        return viewer.identify() + "\n" +
+                movie.identify() + "\n" +
+                this.identify() + "\n\n" +
+                "--- Transaction Summary ---\n" +
+                viewer.getDetails() + "\n" +
+                "Movie: " + movie.title + " (P" + calculateTotal() + ")\n" +
+                "Ticket ID: " + ticketId;
+    }
 }
